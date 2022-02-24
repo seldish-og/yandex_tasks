7ff59bf532577de9894f9e500fa98b712be1be5f
seldish-og <jegorbasya@mail.ru>
2022-02-24 17:32:44 +0300

    sd

--- SQLALCHEMY_Task1/data/users.py
+++ SQLALCHEMY_Task1/data/users.py
@@ -1,30 +1,26 @@
 import datetime
+from email.policy import default
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
 
 
-# team_leader (id) (id руководителя, целое число)
-# job (description) (описание работы)
-# work_size (hours) (объем работы в часах)
-# collaborators (list of id of participants) (список id участников)
-# start_date (дата начала)
-# end_date (дата окончания)
-# is_finished (bool) (признак завершения)
-
 class Jobs(SqlAlchemyBase):
-    __tablename__ = 'jobs'
+    __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    team_leader = sqlalchemy.Column(
+        sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.DateTime,
                                   default=datetime.datetime.now)
 
-    collaborators = sqlalchemy.Column(sqlalchemy.ARRAY, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                    default=datetime.datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                  default=datetime.datetime.now)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user = orm.relation("User")
